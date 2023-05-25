c67fc42d68cbc8c7272f5c6536531c7e2e573e0d
arnau <arnauval17@gmail.com>
2023-05-25 12:21:06 +0200

    first commit

---
+++ BANC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.banc;
+
+/**
+ *
+ * @author arnau
+ */
+public class BANC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+"# M8-UF5" 
